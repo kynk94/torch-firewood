5e17215cce9c9033faa0782d6fc4ff3cc409c491
kynk94 <kynk94@naver.com>
2022-09-02 15:38:35 +0900

    To remove ambiguity, rename the weight layer from 'weight' to 'weighting'.

--- firewood/layers/block.py
+++ firewood/layers/block.py
@@ -17,7 +17,7 @@ from firewood.layers.upfirdn import get_upfirdn_layer
 
 # If want to use other layers in Block, modify values of SUPPORT_LAYER_NAMES.
 SUPPORT_LAYER_NAMES = {
-    "W": ["up_fir", "weight", "down_fir", "noise"],
+    "W": ["up_fir", "weighting", "down_fir", "noise"],
     "N": ["normalization"],
     "B": ["bias"],
     "A": ["activation", "dropout"],
@@ -29,7 +29,7 @@ class Block(nn.Module):
     Wrapper for the weight layer.
 
     Basic order of layers:
-        (weight -> fir -> noise) -> normalization -> (bias -> activation)
+        (weighting -> fir -> noise) -> normalization -> (bias -> activation)
         Each layers is optional, except weight layer.
     """
 
@@ -63,7 +63,7 @@ class Block(nn.Module):
         self.layers = nn.ModuleDict()
 
         # set weight layer
-        self.update_layer_in_order("weight", weight_layer)
+        self.update_layer_in_order("weighting", weight_layer)
 
         # set FIR filter
         up_fir_layer, down_fir_layer = get_upfirdn_layer(
@@ -113,7 +113,7 @@ class Block(nn.Module):
         for _norm in self.weight_normalization:
             norm = weight_normalizations.get(_norm, **weight_normalization_args)
             if norm is not None:
-                norm(self.layers.get_submodule("weight"))
+                norm(self.layers.get_submodule("weighting"))
 
         if lr_equalization is None:
             lr_equalization = backend.lr_equalization()
@@ -143,7 +143,7 @@ class Block(nn.Module):
         self.__lr_equalization = value
         self._check_layers()
 
-        weight_layer = self.layers.get_submodule("weight")
+        weight_layer = self.layers.get_submodule("weighting")
         activation_layer = getattr(self.layers, "activation", None)
         bias_layer = getattr(self.layers, "bias", None)
         if value:
@@ -223,9 +223,9 @@ class Block(nn.Module):
         self.layers = updated_layers
 
     def __move_bias_to_independent_layer(self) -> None:
-        if "weight" not in self.layers:
+        if "weighting" not in self.layers:
             return
-        weight_layer = self.layers.get_submodule("weight")
+        weight_layer = self.layers.get_submodule("weighting")
         bias_attrs = lr_equalizers.pop_bias_attrs(weight_layer)
         if bias_attrs["bias"] is None:
             return
@@ -241,6 +241,10 @@ class Block(nn.Module):
             activation_layer = self.layers.get_submodule("activation")
             if getattr(activation_layer, "bias", None) is not None:
                 lr_equalizers.pop_bias_attrs(activation_layer)
+                print(
+                    "Duplicated bias is removed which is in activation layer. "
+                    "And keep the bias layer."
+                )
 
         # If affine is True, the normalization layer multiply weight and
         # add bias. So, no need to use bias after normalization.
@@ -248,6 +252,7 @@ class Block(nn.Module):
             normalization_layer = self.layers.get_submodule("normalization")
             if getattr(normalization_layer, "affine", False):
                 self._update_layer_in_order("bias", None)
+                print("Meaningless bias before normalization layer removed.")
                 return
 
     def __update_activation(self, fuse: bool) -> None:
@@ -325,11 +330,11 @@ class Block(nn.Module):
         """
         if "bias" not in self.layers:
             return
-        index = SUPPORT_LAYER_NAMES["W"].index("weight")
+        index = SUPPORT_LAYER_NAMES["W"].index("weighting")
         for layer in SUPPORT_LAYER_NAMES["W"][index + 1 :]:
             if layer in self.layers:
                 return
-        weight_layer = self.layers.get_submodule("weight")
+        weight_layer = self.layers.get_submodule("weighting")
         bias_layer = self.layers.get_submodule("bias")
         lr_equalizers.transfer_bias_attrs(bias_layer, weight_layer)
         self._update_layer_in_order("bias", None)
