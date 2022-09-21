694fd39d56a96d7a03328ad5ae5925e307b0ade9
kynk94 <kynk94@naver.com>
2022-09-21 17:07:51 +0900

    add NUMBER type

--- firewood/common/types.py
+++ firewood/common/types.py
@@ -1,12 +1,15 @@
 from typing import Literal, Sequence, Union
 
 import torch
+from numpy import ndarray
+from torch import Tensor
 
 DEVICE = Union[str, torch.device]
 
 STR = Union[str, Sequence[str]]
 INT = Union[int, Sequence[int]]
 FLOAT = Union[float, Sequence[float]]
+NUMBER = Union[FLOAT, ndarray, Tensor]
 
 NEST_STR = Union[str, Sequence[str], Sequence[Sequence[str]]]
 NEST_INT = Union[int, Sequence[int], Sequence[Sequence[int]]]
